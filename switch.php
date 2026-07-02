39560401868d13ff39a4bbb6bba73c451be6b547
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-02 22:22:20 +0530

    switch.php

---
+++ switch.php
@@ -0,0 +1,23 @@
+<?php
+$grade = "A";
+switch ($grade) {
+    case "A":
+        echo "You are topper";
+        break;
+
+    case "B":
+        echo "Verry well";
+        break;
+
+    case "C":
+        echo "Good";
+        break;
+
+    case "D":
+        echo "Can do better";
+        break;
+
+    case "F":
+        echo "You failled";
+        break;
+}
