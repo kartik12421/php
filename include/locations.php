f6f9220e3009fda6fc6d9b1aa69d947c387b1f34
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-07-04 01:09:54 +0530

    include function in php

---
+++ include/locations.php
@@ -0,0 +1,21 @@
+<?php
+include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locations</title>
+</head>
+
+<body>
+    this is locations page <br>
+    stuff about your locations page <br>
+</body>
+
+</html>
+<?php
+include("footer.html");
+?>
